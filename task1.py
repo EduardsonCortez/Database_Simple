9c905ee31d15669d2cae358af47bda8bc533053d
EDCOR <eduardcortez0528@gmail.com>
2025-10-03 09:31:38 +0800

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,13 @@
+print("\n")
+print("================================================")
+print("            DATABASE BY EDUARD CORTEZ                                    ")
+print("================================================")
+
+print("\n")
+
+name = input("Enter your name: ")
+age = input("Enter Age: ")
+favhobby = input("Enter Favorite Hobby: ")
+
+print("================================================")
+print("\nName: " + name + ".\nAGE " + age + ".\nfavorite hobby: " + favhobby + ".")
